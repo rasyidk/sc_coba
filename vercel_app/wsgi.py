d896dbe92d33f9c2d459662c1629fcee247c6355
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-08-20 17:19:04 +0700

    fix server

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -1,16 +1,16 @@
 """
 WSGI config for vercel_app project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vercel_app.settings")
 
-application = get_wsgi_application()
+app = get_wsgi_application()
